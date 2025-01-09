61c8f094b555b0f2ae0310487ad5253d97766c42
sulistyo <sulistyo.wahyu27@gmail.com>
2025-01-09 13:05:37 +0700

    add onclick on listview

--- lib/presentation/widgets/list_image_card.dart
+++ lib/presentation/widgets/list_image_card.dart
@@ -1,4 +1,5 @@
 import 'package:awesome_app/domain/entities/photo.dart';
+import 'package:awesome_app/utils/routes.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -10,42 +11,55 @@ class ListImageCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            flex: 1,
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(14), bottomLeft: Radius.circular(10)),
-              child: CachedNetworkImage(
-                height: 80,
-                width: 100,
-                imageUrl: photo.src!.medium!,
-                fit: BoxFit.cover,
-                placeholder: (context, url) => Center(
-                  child: CircularProgressIndicator(),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, DETAIL_PAGE_ROUTE, arguments: photo.id);
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Flexible(
+              flex: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(14),
+                    bottomLeft: Radius.circular(10)),
+                child: CachedNetworkImage(
+                  height: 80,
+                  width: 100,
+                  imageUrl: photo.src!.medium!,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
                 ),
-                errorWidget: (context, url, error) => Icon(Icons.error),
               ),
             ),
-          ),
-          Flexible(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("by ${photo.photographer!}", style: TextStyle(color: Colors.black),),
-                  Text(photo.alt!, overflow: TextOverflow.ellipsis,style: TextStyle(fontSize: 12),),
-                ],
+            Flexible(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "by ${photo.photographer!}",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Text(
+                      photo.alt!,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
